eef4957635b5e800611bf77c5a608d8f2b78be72
Jbristhuille <jbristhuille@gmail.com>
2025-07-04 13:44:40 +0200

    Add languages section to app
    Introduced a new LanguagesSection component with HTML, SCSS, and TypeScript files. Updated app module and template to include the new section. Added relevant French translations for language labels and proficiency levels.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,20 +2,22 @@
  * @Author                : Jbristhuille<jbristhuille@gmail.com>             *
  * @CreatedDate           : 2025-07-03 21:09:09                              *
  * @LastEditors           : Jbristhuille<jbristhuille@gmail.com>             *
- * @LastEditDate          : 2025-07-04 13:30:32                              *
+ * @LastEditDate          : 2025-07-04 13:43:48                              *
  ****************************************************************************/
 
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { ExperiencesSection } from './sections/experiences/experiences.section';
 import { ProfileSection } from './sections/profile/profile.section';
+import { LanguagesSection } from './sections/languages/languages.section';
 
 @Component({
   selector: 'app-root',
   imports: [
     TranslateModule,
     ExperiencesSection,
-    ProfileSection
+    ProfileSection,
+    LanguagesSection
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
---
+++ src/app/sections/languages/languages.section.ts
@@ -0,0 +1,19 @@
+/*****************************************************************************
+ * @Author                : Jbristhuille<jbristhuille@gmail.com>             *
+ * @CreatedDate           : 2025-07-04 13:42:21                              *
+ * @LastEditors           : Jbristhuille<jbristhuille@gmail.com>             *
+ * @LastEditDate          : 2025-07-04 13:42:22                              *
+ ****************************************************************************/
+
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'section-languages',
+  imports: [TranslateModule],
+  templateUrl: './languages.section.html',
+  styleUrl: './languages.section.scss'
+})
+export class LanguagesSection {
+
+}
