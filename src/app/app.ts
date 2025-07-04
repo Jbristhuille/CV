1fd0349bf7c262aa70c44d66ec8ac0a606c47889
Jbristhuille <jbristhuille@gmail.com>
2025-07-04 14:00:24 +0200

    Add education section to app
    Introduced a new Education section with its component, template, and styles. Updated the main app component to include the new section and added the 'EDUCATION' translation key to the French locale.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@
  * @Author                : Jbristhuille<jbristhuille@gmail.com>             *
  * @CreatedDate           : 2025-07-03 21:09:09                              *
  * @LastEditors           : Jbristhuille<jbristhuille@gmail.com>             *
- * @LastEditDate          : 2025-07-04 13:50:06                              *
+ * @LastEditDate          : 2025-07-04 13:57:53                              *
  ****************************************************************************/
 
 import { Component } from '@angular/core';
@@ -11,6 +11,7 @@ import { ExperiencesSection } from './sections/experiences/experiences.section';
 import { ProfileSection } from './sections/profile/profile.section';
 import { LanguagesSection } from './sections/languages/languages.section';
 import { SkillsSection } from './sections/skills/skills.section';
+import { EducationSection } from './sections/education/education.section';
 
 @Component({
   selector: 'app-root',
@@ -19,7 +20,8 @@ import { SkillsSection } from './sections/skills/skills.section';
     ExperiencesSection,
     ProfileSection,
     LanguagesSection,
-    SkillsSection
+    SkillsSection,
+    EducationSection
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
---
+++ src/app/sections/education/education.section.ts
@@ -0,0 +1,19 @@
+/*****************************************************************************
+ * @Author                : Jbristhuille<jbristhuille@gmail.com>             *
+ * @CreatedDate           : 2025-07-04 13:58:49                              *
+ * @LastEditors           : Jbristhuille<jbristhuille@gmail.com>             *
+ * @LastEditDate          : 2025-07-04 13:58:59                              *
+ ****************************************************************************/
+
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'section-education',
+  imports: [TranslateModule],
+  templateUrl: './education.section.html',
+  styleUrl: './education.section.scss'
+})
+export class EducationSection {
+
+}
