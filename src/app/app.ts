2821861fc34f14943c0e5871da52d1c790bcafd2
Jbristhuille <jbristhuille@gmail.com>
2025-07-04 13:54:25 +0200

    Add skills section to application
    Introduced a new Skills section with its component, template, and styles. Updated the main app template and module to include the new section. Added French translation for the 'SKILLS' label.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@
  * @Author                : Jbristhuille<jbristhuille@gmail.com>             *
  * @CreatedDate           : 2025-07-03 21:09:09                              *
  * @LastEditors           : Jbristhuille<jbristhuille@gmail.com>             *
- * @LastEditDate          : 2025-07-04 13:43:48                              *
+ * @LastEditDate          : 2025-07-04 13:50:06                              *
  ****************************************************************************/
 
 import { Component } from '@angular/core';
@@ -10,6 +10,7 @@ import { TranslateModule } from '@ngx-translate/core';
 import { ExperiencesSection } from './sections/experiences/experiences.section';
 import { ProfileSection } from './sections/profile/profile.section';
 import { LanguagesSection } from './sections/languages/languages.section';
+import { SkillsSection } from './sections/skills/skills.section';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,8 @@ import { LanguagesSection } from './sections/languages/languages.section';
     TranslateModule,
     ExperiencesSection,
     ProfileSection,
-    LanguagesSection
+    LanguagesSection,
+    SkillsSection
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
---
+++ src/app/sections/skills/skills.section.ts
@@ -0,0 +1,34 @@
+/*****************************************************************************
+ * @Author                : Jbristhuille<jbristhuille@gmail.com>             *
+ * @CreatedDate           : 2025-07-04 13:49:25                              *
+ * @LastEditors           : Jbristhuille<jbristhuille@gmail.com>             *
+ * @LastEditDate          : 2025-07-04 13:53:37                              *
+ ****************************************************************************/
+
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+interface ISkills {
+  title: string,
+  skills: string[]
+}
+
+@Component({
+  selector: 'section-skills',
+  imports: [TranslateModule, CommonModule],
+  templateUrl: './skills.section.html',
+  styleUrl: './skills.section.scss'
+})
+export class SkillsSection {
+  public skills: ISkills[] = [{
+    title: "Frontend",
+    skills: ["Angular", "React", "HTML5", "CSS3", "Sass", "Bootstrap", "Tailwind CSS"]
+  }, {
+    title: "Backend",
+    skills: ["Node.js", "Express", "NestJS", "Spring Boot", "NextJS"]
+  }, {
+    title: "Languages",
+    skills: ["Typescript", "JavaScript", "Python", "Java", "Rust"]
+  }]
+}
