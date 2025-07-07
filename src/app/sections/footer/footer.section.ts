2dac5c8de5a57d8a973795e10e084f9da6dad49f
Jbristhuille <jbristhuille@gmail.com>
2025-07-07 15:21:14 +0200

    Add CV download button to footer section
    Introduced a download button in the footer to allow users to download the CV PDF. Updated the footer layout and styles, added the downloadCV method in the component, included the PDF asset, and updated i18n files with the new 'DOWNLOAD' label.

--- src/app/sections/footer/footer.section.ts
+++ src/app/sections/footer/footer.section.ts
@@ -2,7 +2,7 @@
  * @Author                : Jbristhuille<jbristhuille@gmail.com>             *
  * @CreatedDate           : 2025-07-05 13:57:14                              *
  * @LastEditors           : Jbristhuille<jbristhuille@gmail.com>             *
- * @LastEditDate          : 2025-07-05 14:09:21                              *
+ * @LastEditDate          : 2025-07-07 15:17:21                              *
  ****************************************************************************/
 
 import { Component } from '@angular/core';
@@ -29,5 +29,13 @@ export class FooterSection {
 
   onLanguageChange(lang: string) {
     this.i18n.setLanguage(lang);
+  }
+
+  downloadCV(): void {
+    const link = document.createElement('a');
+    link.href = '/Jean-Baptiste_Bristhuille_CV_Freelance.pdf';
+    link.download = 'Jean-Baptiste_Bristhuille_CV_Freelance.pdf';
+    link.target = '_blank';
+    link.click();
   }
 }
